1a1dbb64e168b50db209dbfe25ae54acf40729bd
Roman Petrov <roman.petrov@travelline.ru>
2026-01-29 12:06:18 +0300

    improve configuration

--- prettier.config.js
+++ prettier.config.js
@@ -2,6 +2,7 @@ export default {
   arrowParens: `avoid`,
   endOfLine: `crlf`,
   overrides: [{ files: [`*.{ts,js,tsx}`], options: { objectWrap: `collapse` } }],
+  plugins: [`prettier-plugin-pkg`],
   printWidth: 120,
   proseWrap: `always`,
   quoteProps: `consistent`,
