1e1f48f928bd90e356cbe9b556a3616dd7445309
Roman Petrov <romanpetrov@list.ru>
2026-02-08 02:45:45 +0300

    improve HttpServer

--- packages/server/src/HttpServer.ts
+++ packages/server/src/HttpServer.ts
@@ -19,29 +19,40 @@ export type HttpServerOptions = {
 type RequestHandler = (request: IncomingMessage, response: ServerResponse) => void;
 
 export const HttpServer = (options: HttpServerOptions) => {
-  const start = (): void => {
-    const { root } = options;
-    const serve = serveStatic(root);
-    const { middleware, sslCertPem: certPem, sslKeyPem: keyPem } = options;
-    const useHttps = certPem !== undefined && keyPem !== undefined;
-    const port = useHttps ? portHttps : portHttp;
+  const { middleware, root, sslCertPem: certPem, sslKeyPem: keyPem } = options;
+  const useHttps = certPem !== undefined && keyPem !== undefined;
+  const port = useHttps ? portHttps : portHttp;
+  const serve = serveStatic(root);
 
-    const baseHandler: RequestHandler = (request, response) => {
-      serve(request, response, () => {
-        response.statusCode = 404;
-        response.end();
-      });
-    };
+  const baseHandler: RequestHandler = (request, response) => {
+    serve(request, response, () => {
+      response.statusCode = 404;
+      response.end();
+    });
+  };
 
-    const handler = middleware === undefined ? baseHandler : middleware.createHandler(root, baseHandler);
-    const server = useHttps ? https.createServer({ cert: certPem, key: keyPem }, handler) : http.createServer(handler);
+  const handler = middleware === undefined ? baseHandler : middleware.createHandler(root, baseHandler);
+  const server = useHttps ? https.createServer({ cert: certPem, key: keyPem }, handler) : http.createServer(handler);
 
+  const start = () => {
     server.listen(port, () => {
       process.stdout.write(`🌐 Site server started on port ${port} (${useHttps ? `HTTPS` : `HTTP`})\n`);
     });
   };
 
-  return { start };
+  const stop = async () =>
+    new Promise<void>(resolve => {
+      server.close(error => {
+        if (error === undefined) {
+          process.stdout.write(`🌐 Site server stopped\n`);
+        } else {
+          process.stderr.write(`${error.message}\n`);
+        }
+        resolve();
+      });
+    });
+
+  return { start, stop };
 };
 
 export type HttpServer = ReturnType<typeof HttpServer>;
