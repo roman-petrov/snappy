fcc704863d14bfbae9556b24767d60a65ba0af8d
Roman Petrov <romanpetrov@list.ru>
2026-02-13 10:22:45 +0300

    simplify

--- packages/snappy-site/src/site/components/Who.tsx
+++ packages/snappy-site/src/site/components/Who.tsx
@@ -4,15 +4,13 @@ import { WhoItem } from "./WhoItem";
 
 export const Who = () => (
   <Section id="who" lead={t(`who.lead`)} title={t(`who.title`)}>
-    <div>
-      {[
-        { icon: `📱`, key: `who.smm` as const },
-        { icon: `✉️`, key: `who.emails` as const },
-        { icon: `📄`, key: `who.ads` as const },
-        { icon: `🎓`, key: `who.study` as const },
-      ].map(({ icon, key }) => (
-        <WhoItem description={t(`${key}.desc`)} icon={icon} key={key} title={t(`${key}.title`)} />
-      ))}
-    </div>
+    {[
+      { icon: `📱`, key: `who.smm` as const },
+      { icon: `✉️`, key: `who.emails` as const },
+      { icon: `📄`, key: `who.ads` as const },
+      { icon: `🎓`, key: `who.study` as const },
+    ].map(({ icon, key }) => (
+      <WhoItem description={t(`${key}.desc`)} icon={icon} key={key} title={t(`${key}.title`)} />
+    ))}
   </Section>
 );
