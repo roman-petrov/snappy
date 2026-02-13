9468cb07c45c379fa70c3d8ccc56b67911200020
Roman Petrov <romanpetrov@list.ru>
2026-02-13 18:07:29 +0300

    fix lint

--- packages/snappy-site/src/site/components/CardWithIcon.tsx
+++ packages/snappy-site/src/site/components/CardWithIcon.tsx
@@ -3,8 +3,8 @@ import styles from "./CardWithIcon.module.css";
 export type CardWithIconProps = { description: string; icon: string; title: string; variant: `feature` | `who` };
 
 const variantStyles: { feature: { icon: string; wrapper: string }; who: { icon: string; wrapper: string } } = {
-  feature: { icon: styles[`featureIcon`] ?? ``, wrapper: styles[`feature`] ?? `` },
-  who: { icon: styles[`whoIcon`] ?? ``, wrapper: styles[`who`] ?? `` },
+  feature: { icon: styles.featureIcon, wrapper: styles.feature },
+  who: { icon: styles.whoIcon, wrapper: styles.who },
 };
 
 export const CardWithIcon = ({ description, icon, title, variant }: CardWithIconProps) => {
