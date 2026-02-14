49379c5fc6dcbc5cc37e45f7f38d255772e119e7
Roman Petrov <romanpetrov@list.ru>
2026-02-14 17:11:31 +0300

    simplify

--- packages/snappy-site/src/site/components/CardWithIcon.tsx
+++ packages/snappy-site/src/site/components/CardWithIcon.tsx
@@ -2,17 +2,15 @@ import styles from "./CardWithIcon.module.css";
 
 export type CardWithIconProps = { description: string; icon: string; title: string; variant: `feature` | `who` };
 
-const variantStyles: { feature: { icon: string; wrapper: string }; who: { icon: string; wrapper: string } } = {
-  feature: { icon: styles.featureIcon, wrapper: styles.feature },
-  who: { icon: styles.whoIcon, wrapper: styles.who },
-};
-
 export const CardWithIcon = ({ description, icon, title, variant }: CardWithIconProps) => {
-  const s = variantStyles[variant];
+  const variantStyles: { feature: { icon: string; wrapper: string }; who: { icon: string; wrapper: string } } = {
+    feature: { icon: styles.featureIcon, wrapper: styles.feature },
+    who: { icon: styles.whoIcon, wrapper: styles.who },
+  };
 
   return (
-    <div className={s.wrapper}>
-      <span className={s.icon}>{icon}</span>
+    <div className={variantStyles[variant].wrapper}>
+      <span className={variantStyles[variant].icon}>{icon}</span>
       <h3>{title}</h3>
       <p>{description}</p>
     </div>
