ac02d88a929605188d10dc53ce1803a056b847e3
Roman Petrov <romanpetrov@list.ru>
2026-02-16 16:52:20 +0300

    fix tests & lints

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,8 @@
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  test: { projects: [{ extends: true, test: { include: [`packages/**/*.test.ts`], name: `unit` } }] },
+  test: {
+    environment: `jsdom`,
+    projects: [{ extends: true, test: { include: [`packages/**/*.test.ts`], name: `unit` } }],
+  },
 });
