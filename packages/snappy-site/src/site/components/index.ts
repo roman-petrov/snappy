fd27b20d213d5b0c50fc2f6a6b56b7f067569d40
Roman Petrov <romanpetrov@list.ru>
2026-02-20 16:59:19 +0300

    fix lint

--- packages/snappy-site/src/site/components/index.ts
+++ packages/snappy-site/src/site/components/index.ts
@@ -6,8 +6,6 @@ export * from "./Faq";
 
 export * from "./Features";
 
-export * from "./SiteHeader";
-
 export * from "./Hero";
 
 export * from "./Landing";
@@ -15,6 +13,8 @@ export * from "./Landing";
 export * from "./Section";
 
 export * from "./SiteFooter";
+
+export * from "./SiteHeader";
 
 export * from "./Steps";
 
