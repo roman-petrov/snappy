d472e4fef1b9d39b957ae40c6bc4a9d3a3011ff6
Roman Petrov <romanpetrov@list.ru>
2026-02-20 10:06:02 +0300

    fix line endings

--- packages/do/src/config/prettier/PrettierConfig.ts
+++ packages/do/src/config/prettier/PrettierConfig.ts
@@ -1,6 +1,5 @@
 export const PrettierConfig = {
   arrowParens: `avoid`,
-  endOfLine: `lf`,
   overrides: [{ files: [`*.{ts,js,tsx}`], options: { objectWrap: `collapse` } }],
   plugins: [`prettier-plugin-pkg`],
   printWidth: 120,
