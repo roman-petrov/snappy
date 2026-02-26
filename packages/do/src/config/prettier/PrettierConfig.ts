95b60faf7dde207b1185c7bbb8e7226b5827a9fc
Roman Petrov <romanpetrov@list.ru>
2026-02-26 08:58:06 +0300

    fix prettier config

--- packages/do/src/config/prettier/PrettierConfig.ts
+++ packages/do/src/config/prettier/PrettierConfig.ts
@@ -1,5 +1,6 @@
 export const PrettierConfig = {
   arrowParens: `avoid`,
+  endOfLine: `auto`,
   overrides: [{ files: [`*.{ts,js,tsx}`], options: { objectWrap: `collapse` } }],
   plugins: [`prettier-plugin-pkg`],
   printWidth: 120,
