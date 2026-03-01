73a14721e010be2099570a94e3643c32a3e46135
Roman Petrov <romanpetrov@list.ru>
2026-03-01 16:18:23 +0300

    fix tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  resolve: { dedupe: [`react`, `react-dom`] },
   test: {
     environment: `jsdom`,
     projects: [{ extends: true, test: { include: [`packages/**/*.test.ts`], name: `unit` } }],
