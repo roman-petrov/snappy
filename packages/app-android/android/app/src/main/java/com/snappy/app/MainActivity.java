bf03a3bc39fe8b77d6113b77ced2ecc56e52d2cf
Roman Petrov <romanpetrov@list.ru>
2026-03-09 11:25:41 +0300

    fix keyboard in Android app

--- packages/app-android/android/app/src/main/java/com/snappy/app/MainActivity.java
+++ packages/app-android/android/app/src/main/java/com/snappy/app/MainActivity.java
@@ -9,6 +9,9 @@ import androidx.activity.ComponentActivity;
 import androidx.activity.EdgeToEdge;
 import androidx.activity.OnBackPressedCallback;
 import androidx.activity.SystemBarStyle;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends ComponentActivity {
 
@@ -22,7 +25,14 @@ public class MainActivity extends ComponentActivity {
         );
         setContentView(R.layout.activity_main);
 
+        android.view.View container = findViewById(R.id.webview_container);
         WebView webView = findViewById(R.id.webview);
+        ViewCompat.setOnApplyWindowInsetsListener(container, (v, windowInsets) -> {
+            Insets ime = windowInsets.getInsets(WindowInsetsCompat.Type.ime());
+            v.setPadding(0, 0, 0, ime.bottom);
+            return windowInsets;
+        });
+        container.requestApplyInsets();
         WebSettings settings = webView.getSettings();
         settings.setJavaScriptEnabled(true);
         settings.setDomStorageEnabled(true);
