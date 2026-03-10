bf101c9fca46607904365acee275da3edf976f4e
Roman Petrov <romanpetrov@list.ru>
2026-03-11 00:22:11 +0300

    improve styles

--- packages/ui/src/components/Logo.module.scss.d.ts
+++ packages/ui/src/components/Logo.module.scss.d.ts
@@ -1,5 +1,6 @@
 declare const __Logo: {
   readonly logo: "logo";
   readonly logoIcon: "logoIcon";
+  readonly logoText: "logoText";
 };
 export default __Logo;
--- packages/ui/src/components/Logo.tsx
+++ packages/ui/src/components/Logo.tsx
@@ -8,6 +8,6 @@ export const Logo = ({ title }: LogoProps = {}) => (
   <a className={styles.logo} href="/" title={title}>
     <img alt="" aria-hidden="true" className={styles.logoIcon} height={20} src={faviconUrl} width={20} />
     {` `}
-    <Text as="span" color="accent" text="Snappy" typography="h3" />
+    <Text as="span" cn={styles.logoText} color="accent" text="Snappy" />
   </a>
 );
