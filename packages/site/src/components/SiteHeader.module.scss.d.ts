8663b6d09d72f4c018d8828b9657b2d136a49bbf
Roman Petrov <romanpetrov@list.ru>
2026-03-10 23:10:11 +0300

    fix header on mobile

---
+++ packages/site/src/components/SiteHeader.module.scss.d.ts
@@ -0,0 +1,4 @@
+declare const __SiteHeader: {
+  readonly root: "root";
+};
+export default __SiteHeader;
--- packages/site/src/components/SiteHeader.view.tsx
+++ packages/site/src/components/SiteHeader.view.tsx
@@ -3,14 +3,17 @@ import { Header, Link, SystemButtons } from "@snappy/ui";
 import type { useSiteHeaderState } from "./SiteHeader.state";
 
 import { t } from "../core";
+import styles from "./SiteHeader.module.scss";
 
 export type SiteHeaderViewProps = ReturnType<typeof useSiteHeaderState>;
 
 export const SiteHeaderView = ({ navItems }: SiteHeaderViewProps) => (
-  <Header>
-    {navItems.map(({ href, key }) => (
-      <Link href={href} key={key} muted text={t(key)} />
-    ))}
-    <SystemButtons />
-  </Header>
+  <div className={styles.root}>
+    <Header>
+      {navItems.map(({ href, key }) => (
+        <Link href={href} key={key} muted text={t(key)} />
+      ))}
+      <SystemButtons />
+    </Header>
+  </div>
 );
