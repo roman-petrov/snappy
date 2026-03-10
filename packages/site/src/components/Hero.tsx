9da311f3210321cca276cefc04d823cb63801cb0
Roman Petrov <romanpetrov@list.ru>
2026-03-10 23:34:58 +0300

    improve site

--- packages/site/src/components/Hero.tsx
+++ packages/site/src/components/Hero.tsx
@@ -1,10 +1,10 @@
-import { Button, Headline, Link } from "@snappy/ui";
+import { Button, Headline } from "@snappy/ui";
 
 import { t } from "../core";
 
 export const Hero = () => (
   <Headline as="h1" lead={t(`hero.lead`)} title={t(`hero.title`)} variant="hero">
     <Button href="https://t.me/sn4ppy_bot" icon="telegram" large text={t(`hero.cta`)} type="primary" />
-    <Link href="/download/snappy.apk" text={t(`hero.androidApp`)} />
+    <Button href="/download/snappy.apk" text={t(`hero.androidApp`)} type="link" />
   </Headline>
 );
