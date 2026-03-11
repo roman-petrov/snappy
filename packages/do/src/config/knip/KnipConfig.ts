8dfbad368f633f0c38bb5add12999f070e23b40b
Roman Petrov <romanpetrov@list.ru>
2026-03-11 13:34:24 +0300

    fix knip

--- packages/do/src/config/knip/KnipConfig.ts
+++ packages/do/src/config/knip/KnipConfig.ts
@@ -14,9 +14,9 @@ const config: KnipConfig = {
       entry: [`cspell.config.js`, `eslint.config.js`, `prettier.config.js`, `stylelint.config.js`, `knip.config.ts`],
       ignoreDependencies: [`@knip/mcp`, `jsdom`, `actions-up`],
     },
-    "packages/app": { ignoreDependencies: [`@snappy/do`, `preact`] },
+    "packages/app": { ignoreDependencies: [`@snappy/do`] },
     "packages/app-desktop": { entry: [`index.html`], ignoreDependencies: [`preact`] },
-    "packages/app-mobile": { entry: [`index.html`], ignoreDependencies: [`preact`] },
+    "packages/app-mobile": { entry: [`index.html`] },
     "packages/db": { entry: [`src/Seed.ts`] },
     "packages/do": {
       entry: [`src/main.*.ts`],
@@ -30,11 +30,10 @@ const config: KnipConfig = {
         `prettier-plugin-pkg`,
         `stylelint-config-recess-order`,
         `stylelint-config-standard-scss`,
-        `vitest`,
       ],
     },
     "packages/site": { entry: [`index.html`, `src/entry-server.tsx`], ignoreDependencies: [`@types/react-dom`] },
-    "packages/ui": { ignoreDependencies: [`@fontsource-variable/inter`, `preact`] },
+    "packages/ui": { ignoreDependencies: [`@fontsource-variable/inter`] },
   },
 };
 
