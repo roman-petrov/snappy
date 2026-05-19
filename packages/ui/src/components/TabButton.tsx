2423dedfdf29a719e9cf88ad84787f25884096f1
Roman Petrov <romanpetrov@list.ru>
2026-05-19 15:02:43 +0300

    fix tabs

--- packages/ui/src/components/TabButton.tsx
+++ packages/ui/src/components/TabButton.tsx
@@ -1,8 +1,17 @@
-import type { TapProps } from "./Tap";
+import { _ } from "@snappy/core";
 
-import { useTabButtonState } from "./TabButton.state";
-import { TabButtonView } from "./TabButton.view";
+import { $ } from "../$";
+import styles from "./TabButton.module.scss";
+import { Tap, type TapProps } from "./Tap";
 
 export type TabButtonProps = Omit<TapProps, `children` | `cn`> & { active?: boolean; text: string };
 
-export const TabButton = (props: TabButtonProps) => <TabButtonView {...useTabButtonState(props)} />;
+export const TabButton = ({ active = false, onClick, text, ...rest }: TabButtonProps) => (
+  <Tap
+    {...rest}
+    children={text}
+    cn={_.cn($.radius(`sm`), $.typography(`caption`), styles.tab, active ? $.tap(`accent`) : $.tap(`soft`))}
+    onClick={onClick}
+    vibrate="segmentTick"
+  />
+);
