5334da353ef2ef83eb755e83e9ce53d214a928e9
Roman Petrov <romanpetrov@list.ru>
2026-06-01 12:31:23 +0300

    improve ux

--- packages/app/src/snappy/components/StaticForm.view.tsx
+++ packages/app/src/snappy/components/StaticForm.view.tsx
@@ -3,23 +3,20 @@ import { Button } from "@snappy/ui";
 import type { useStaticFormState } from "./StaticForm.state";
 
 import { t } from "../../locales";
-import { FeedPanel } from "./FeedPanel";
 import styles from "./StaticForm.module.scss";
 import { StaticFormField } from "./StaticFormField";
 
 export type StaticFormViewProps = ReturnType<typeof useStaticFormState>;
 
 export const StaticFormView = ({ fields, submit }: StaticFormViewProps) => (
-  <FeedPanel>
-    <div className={styles.root}>
-      {fields.map(field => (
-        <StaticFormField key={field.id} label={field.label}>
-          <field.Component {...field.props} />
-        </StaticFormField>
-      ))}
-      <div className={styles.actions}>
-        <Button onClick={submit} text={t(`chat.continue`)} type="primary" />
-      </div>
+  <div className={styles.root}>
+    {fields.map(field => (
+      <StaticFormField key={field.id} label={field.label}>
+        <field.Component {...field.props} />
+      </StaticFormField>
+    ))}
+    <div className={styles.actions}>
+      <Button onClick={submit} text={t(`chat.continue`)} type="primary" />
     </div>
-  </FeedPanel>
+  </div>
 );
--- packages/app/src/snappy/components/StaticFormField.tsx
+++ packages/app/src/snappy/components/StaticFormField.tsx
@@ -1,18 +1,17 @@
 import type { StaticFormLabel } from "@snappy/snappy-sdk";
 import type { ReactNode } from "react";
 
-import { _ } from "@snappy/core";
-import { $ } from "@snappy/ui";
+import { $, Card } from "@snappy/ui";
 
 import styles from "./StaticFormField.module.scss";
 
 export type StaticFormFieldProps = { children: ReactNode; label: StaticFormLabel };
 
 export const StaticFormField = ({ children, label }: StaticFormFieldProps) => (
-  <section className={styles.field}>
-    <div className={_.cn(styles.labelWrap, $.surface(`surface`), $.elevation(`e1`), $.radius(`sm`))}>
+  <Card cn={styles.field}>
+    <div className={styles.labelWrap}>
       <span className={$.typography(`captionBold`)}>{`${label.emoji} ${label.text}`}</span>
     </div>
     <div className={styles.controls}>{children}</div>
-  </section>
+  </Card>
 );
