c6e556c348d7bfa9bc85e653505127ae43a0b9ce
Roman Petrov <romanpetrov@list.ru>
2026-06-01 12:56:02 +0300

    improve static form

--- packages/app/src/snappy/components/StaticForm.module.scss.d.ts
+++ packages/app/src/snappy/components/StaticForm.module.scss.d.ts
@@ -1,6 +1,7 @@
 declare const __StaticForm: {
   readonly card: "card";
   readonly root: "root";
+  readonly field: "field";
   readonly actions: "actions";
 };
 export default __StaticForm;
--- packages/app/src/snappy/components/StaticFormField.tsx
+++ packages/app/src/snappy/components/StaticFormField.tsx
@@ -1,14 +1,15 @@
 import type { StaticFormLabel } from "@snappy/snappy-sdk";
 import type { ReactNode } from "react";
 
+import { _ } from "@snappy/core";
 import { $ } from "@snappy/ui";
 
 import styles from "./StaticFormField.module.scss";
 
-export type StaticFormFieldProps = { children: ReactNode; label: StaticFormLabel };
+export type StaticFormFieldProps = { children: ReactNode; cn?: string; label: StaticFormLabel };
 
-export const StaticFormField = ({ children, label }: StaticFormFieldProps) => (
-  <section className={styles.field}>
+export const StaticFormField = ({ children, cn, label }: StaticFormFieldProps) => (
+  <section className={_.cn(styles.field, cn)}>
     <div className={styles.labelWrap}>
       <span className={$.typography(`captionBold`)}>{`${label.emoji} ${label.text}`}</span>
     </div>
