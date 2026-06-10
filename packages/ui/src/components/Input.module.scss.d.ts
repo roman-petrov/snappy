a38f4b757fc11e1ded118944d903f4bbca39ba02
Roman Petrov <romanpetrov@list.ru>
2026-06-10 17:57:50 +0300

    improve styles

--- packages/ui/src/components/Input.module.scss.d.ts
+++ packages/ui/src/components/Input.module.scss.d.ts
@@ -4,8 +4,8 @@ declare const __Input: {
   readonly label: "label";
   readonly surface: "surface";
   readonly control: "control";
+  readonly suffix: "suffix";
   readonly hasValue: "hasValue";
   readonly field: "field";
-  readonly suffix: "suffix";
 };
 export default __Input;
