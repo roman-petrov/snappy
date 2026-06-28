041140fede0b88744d9f1dafe6b96829becd84a0
Roman Petrov <romanpetrov@list.ru>
2026-06-28 17:01:55 +0300

    fix mime

--- packages/core/src/Mime.ts
+++ packages/core/src/Mime.ts
@@ -1,6 +1,8 @@
 import { MimeType } from "./MimeType";
 
-const base64 = (bytes: Uint8Array) => Buffer.from(bytes).toString(`base64`);
+const base64 = (bytes: Uint8Array) =>
+  typeof Buffer === `undefined` ? bytes.toBase64() : Buffer.from(bytes).toString(`base64`);
+
 const dataUrl = (type: string, bytes: Uint8Array) => `data:${type};base64,${base64(bytes)}`;
 const pngDataUrl = (bytes: Uint8Array) => dataUrl(MimeType.imagePng, bytes);
 const blob = async (raw: Blob) => dataUrl(raw.type, new Uint8Array(await raw.arrayBuffer()));
