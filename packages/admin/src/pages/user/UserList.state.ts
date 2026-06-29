3863b1b2cc44f6ddc876042d1a39c5f9842ca903
Roman Petrov <romanpetrov@list.ru>
2026-06-29 18:48:52 +0300

    fix admin user list

--- packages/admin/src/pages/user/UserList.state.ts
+++ packages/admin/src/pages/user/UserList.state.ts
@@ -1,16 +1,25 @@
 import type { TrpcOutputs } from "@snappy/admin-server-api";
 
+import { useRouterPath } from "@snappy/app-router";
 import { useAsyncEffect } from "@snappy/ui";
 import { useState } from "react";
 
 import { trpc } from "../../core";
+import { Routes } from "../../Routes";
 
 export const useUserListState = () => {
+  const path = useRouterPath();
+  const active = path === Routes.user.list;
   const pageSize = 20;
   const [page, setPage] = useState(1);
   const [data, setData] = useState<TrpcOutputs[`users`][`list`] | undefined>(undefined);
 
-  useAsyncEffect(async () => setData(await trpc.users.list.query({ page, pageSize })), [page]);
+  useAsyncEffect(async () => {
+    if (!active) {
+      return;
+    }
+    setData(await trpc.users.list.query({ page, pageSize }));
+  }, [active, page]);
 
   const items = data?.items ?? [];
   const total = data?.total ?? 0;
